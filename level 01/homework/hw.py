f3930a91a986cda9d1a08e19abee37e8f5166f8a
DemetreKhosro <demetrexosroshvili@gmail.com>
2026-05-02 00:13:12 +0400

    second commit

---
+++ level 01/homework/hw.py
@@ -0,0 +1,34 @@
+# 2)
+name = 'demetre'
+print(name)
+
+# 3)
+surname = 'khosroshvili'
+print(surname)
+
+# 4)
+age = 15
+print(age)
+
+# 5)
+home = 'avchala'
+print(home)
+
+# 6)
+a = 10
+b = 5
+
+print(a + b)
+print(a + 2)
+print(a - b)
+print(b - a)
+print(a * b)
+print(a * 2)
+print(a / b)
+print(a / 2)
+print(a // 3)
+print(b // 3)
+print(a ** 2)
+print(a ** b)
+print(a % b)
+print(a % 3)
