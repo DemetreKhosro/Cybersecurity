42c17467dacc5c7f7156b5e11947bde2bf371886
DemetreKhosro <demetrexosroshvili@gmail.com>
2026-05-07 19:06:56 +0400

    commit

---
+++ level 03/homework/hw.py
@@ -0,0 +1,47 @@
+# 3) მომხმარებელმა შეიყვანოს სახელი და დაბეჭდე.
+user_name = input("Enter your name: ")
+print(user_name)
+
+
+# 4) მომხმარებელმა შეიყვანოს რიცხვი და დაბეჭდე.
+number = int(input("Enter a number: "))
+print(number)
+
+
+# 5) მომხამრებლმა შეიყვანოს სახელი და დაუმატე 'hello'
+name = input("Enter your name: ")
+print(f"Hello {name}")
+
+
+# 6) შეიყვანე რიცხვი და დაუმატე 1.
+num1 = int(input("Enter a number: "))
+print(num1 + 1)
+
+
+# 7) შეიყვანე 2 რიცხვი და დაბეჭდე ჯამი.
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+print(num2 + num3)
+
+
+# 8) შეიყვანე ათწილადი რიცხვი და დაბეჭდე.
+flt = float(input("Enter a decimal number: "))
+print(flt)
+
+
+# 9) შეიყვანე რიცხვი და გაამრავლე 2-ზე.
+num_to_double = int(input("Enter a number: "))
+print(num_to_double * 2)
+
+
+# 10) მომხმარებელს შემოაყვანინე ინფორმაცია და მოახდინე ყველა ნასწავლ მათემატიკურ ოპერატორებზე მათემატიკური ოპერაციები(+ ; - ; * ; ** ; / ; // ; %)
+num4 = int(input("Enter first number: "))
+num5 = int(input("Enter second number: "))
+
+print(num4 + num5)
+print(num4 - num5)
+print(num4 * num5)
+print(num4 ** num5)
+print(num4 / num5)
+print(num4 // num5)
+print(num4 % num5)
